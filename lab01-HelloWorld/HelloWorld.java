da8db0493bedf436441f46bf30d4a4e9c96c47cd
bgk219 <bgk219@gmail.com>
2015-08-28 19:10:33 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,28 @@
+///////////////////////////////////////
+//Benjamin Kim
+//Demo 0
+//Hello World Java Program
+//
+// first complile the program
+//      javac HellowWorld.java
+// run the program
+//      java HelloWorld//
+
+// define a class
+public class HelloWorld{
+    
+// add main method
+ public static void main(String[] args) {
+     
+// print the statement with the box around it
+   System.out.println("############################");
+   System.out.println("############################");
+   System.out.println("#                          #");   
+   System.out.println("#    Hello World 2015!!    #");
+   System.out.println("#                          #");
+   System.out.println("############################");
+   System.out.println("############################");
+   
+    //test/testtest
+  }
+}
