89008bfd5e34236bf86c6c7a5605c64dcfd3b7bc
bgk219 <bgk219@gmail.com>
2015-09-02 22:25:52 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,26 @@
+//This is Benjamin Kim doing homework... attempt #5
+//Welcome Class Java Program
+//CSE2
+//First Compile the program
+//   javac WelcomeClass.java
+// Run the program
+
+
+public class WelcomeClass {
+    
+    //add main method
+    public static void main(String[] args) {
+        
+//Print the homework please
+System.out.println("--------------");
+System.out.println("|WelcomeClass|");
+System.out.println("--------------");
+System.out.println("  ^  ^  ^  ^  ^  ^");
+System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+System.out.println("<-B--G--K--2--1--9->");
+System.out.println(" \\ /\\ /\\ /\\ /\\ /\\/");
+System.out.println("  v  v  v  v  v  v");
+System.out.println("My first week at lehigh was a wild experience.");
+System.out.println("I met many new people and tried many new things.");
+    }
+}
