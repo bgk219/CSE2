a4105284a8a9a93ec57b0acccf50d61c8e5b9c79
bgk219 <bgk219@gmail.com>
2015-09-16 00:03:37 +0000

    Check.java

---
+++ hw03/Block.java
@@ -0,0 +1,35 @@
+//Benjamin Kim
+//Professor Chen
+//CSE2
+//9/15/15
+//hw03 block
+
+//Use length, width, and height of a block to calculate
+//the volume of the block and the surface area.
+
+//define a class
+public class Block{
+    //main method required for every Java program
+    public static void main(String[] args) {
+    
+//Assign length, width, height, volume, and surface area as doubles
+double length
+double width
+double height
+double volume
+double surfaceArea
+
+//calculate volume by multiplying length, width, and height
+volume= length*width*height
+
+//calculate surface area by multiplying length and width
+surfaceArea=length*width
+
+
+
+//print the completed calculations
+System.out.println(""+)
+
+
+    }
+}
---
+++ hw03/Timer.java
@@ -0,0 +1,45 @@
+//Benjamin Kim
+//Professor Chen
+//CSE2
+//9/15/15
+//hw03 timer
+
+//Use current time and dinner time to 
+//calculate time remaining before dinner.
+
+//Import the Scanner class
+import java.util.Scanner;
+
+//define a class
+public class Timer{
+    //main method required for everey java program
+    public static void main(String[] args) {
+
+//Use the scanner class to obtain the current time
+//and dinner time from the user.
+Scanner myScanner=new Scanner(System.in);
+
+//Prompt the user for entering the current time
+System.out.println("Enter the current time in the form HHMM");
+
+//Accept user input.
+int currentTime=myScanner.nextInt();
+
+//Prompt the user for entering the dinner time.
+System.out.println("Enter the dinner time in the form HHMM");
+
+//Accept user input.
+int dinnerTime=myScanner.nextInt();
+
+//remaining time
+int remainingTime;
+
+//Calculate remaining time by subtracting current time from dinner time.
+remainingTime=dinnerTime-currentTime;
+
+//print the completed calculations
+System.out.println("the amount of time remaining (in the form of HHMM is"+remainingTime);
+    
+        
+    } //end of main method
+} //end of class
